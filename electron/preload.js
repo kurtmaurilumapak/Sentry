56789e9d8ebc1510761d7b0c29ffbb25c938acda
kurtmaurilumapak <kurtmauri.niezas@carsu.edu.ph>
2025-12-24 00:13:00 +0800

    fix: fix electron not working

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,9 @@
-import { contextBridge } from 'electron';
+// Note: Preload scripts run in a special context where Electron expects
+// CommonJS-style modules, so we must use `require` instead of `import`.
+// Using `import` here causes "Cannot use import statement outside a module".
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { contextBridge } = require('electron');
 
 // In this simple prototype we don't need custom Electron APIs yet,
 // but we keep the bridge ready for future extensions (e.g. file system, config).
